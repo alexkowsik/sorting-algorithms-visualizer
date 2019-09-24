6fdb51ae33e0bff58661798bccd059a254c6a4a0
Alexander Kowsik <alexander.kowsik@gmx.de>
2019-09-24 19:59:04 +0200

    added Container component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Container from './components/Container/Container';
+
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
+        <Container />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/Container/Container.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Container.css';
+
+export default class Container extends React.Component {
+  render() {
+    return <div className='container'></div>;
+  }
+}
