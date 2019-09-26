61b35ee8e5bd44f1e0f7a9b5cc622c5324e30563
Alexander Kowsik <alexander.kowsik@gmx.de>
2019-09-26 15:26:20 +0200

    exported algorithms into another module

--- src/components/Bars/Bars.js
+++ src/components/Bars/Bars.js
@@ -3,6 +3,7 @@ import './Bars.css';
 import { Bar } from 'react-chartjs-2';
 
 const shuffle = require('../../utils/algorithms').shuffle;
+const bs = require('../../utils/algorithms').bubbleSort;
 
 class Bars extends React.Component {
   state = {
@@ -23,49 +24,12 @@ class Bars extends React.Component {
     return new Promise(resolve => setTimeout(resolve, milliseconds));
   };
 
-  /*
-   *
-   * -------- SORTING ALGORITHMS --------
-   *
-   */
-  bubbleSort = () => {
-    this.setState({ buttonDisabled: true }, async e => {
-      let chart = this.reference.chartInstance;
-      const dataArray = chart.data.datasets[0].data;
-      const meta = chart.getDatasetMeta(0);
-      const colors = chart.data.datasets[0].backgroundColor;
-      const defaltColor = 'rgb(255, 99, 132)';
-      let tmp;
-
-      for (let i = 0; i < dataArray.length; i++) {
-        for (let j = 0; j < dataArray.length - i - 1; j++) {
-          colors[j] = '#c941fa';
-          chart.data.datasets[0].backgroundColor = colors;
-
-          chart.update();
-          await this.sleep(10);
-
-          if (dataArray[j] > dataArray[j + 1]) {
-            tmp = dataArray[j];
-            dataArray[j] = dataArray[j + 1];
-            dataArray[j + 1] = tmp;
-
-            tmp = meta.data[j];
-            meta.data[j] = meta.data[j + 1];
-            meta.data[j + 1] = tmp;
-
-            colors[j + 1] = '#c941fa';
-          }
-
-          colors[j] = defaltColor;
-          chart.data.datasets[0].backgroundColor = colors;
-        }
-        colors[dataArray.length - i - 1] = '#7cc746';
-        chart.data.datasets[0].backgroundColor = colors;
-        chart.update();
-      }
-      this.arraySorted = true;
-      this.setState({ buttonDisabled: false });
+  bubbleSort = reference => {
+    this.setState({ buttonDisabled: true }, () => {
+      bs(reference).then(() => {
+        this.arraySorted = true;
+        this.setState({ buttonDisabled: false });
+      });
     });
   };
 
@@ -102,7 +66,10 @@ class Bars extends React.Component {
           redraw
         />
 
-        <button onClick={this.bubbleSort} disabled={this.state.buttonDisabled}>
+        <button
+          onClick={() => this.bubbleSort(this.reference)}
+          disabled={this.state.buttonDisabled}
+        >
           Sort
         </button>
         <button onClick={this.shuffleArray} disabled={this.state.buttonDisabled}>
--- src/utils/algorithms.js
+++ src/utils/algorithms.js
@@ -9,4 +9,54 @@ const shuffle = arr => {
   return arr;
 };
 
-exports.shuffle = shuffle;
+const sleep = milliseconds => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
+};
+
+/*
+ *
+ * -------- SORTING ALGORITHMS --------
+ *
+ */
+const bubbleSort = async reference => {
+  let chart = reference.chartInstance;
+  console.log(chart);
+  const dataArray = chart.data.datasets[0].data;
+  const meta = chart.getDatasetMeta(0);
+  const colors = chart.data.datasets[0].backgroundColor;
+  const defaltColor = 'rgb(255, 99, 132)';
+  let tmp;
+
+  for (let i = 0; i < dataArray.length; i++) {
+    for (let j = 0; j < dataArray.length - i - 1; j++) {
+      colors[j] = '#c941fa';
+      chart.data.datasets[0].backgroundColor = colors;
+
+      chart.update();
+      await sleep(10);
+
+      if (dataArray[j] > dataArray[j + 1]) {
+        tmp = dataArray[j];
+        dataArray[j] = dataArray[j + 1];
+        dataArray[j + 1] = tmp;
+
+        tmp = meta.data[j];
+        meta.data[j] = meta.data[j + 1];
+        meta.data[j + 1] = tmp;
+
+        colors[j + 1] = '#c941fa';
+      }
+
+      colors[j] = defaltColor;
+      chart.data.datasets[0].backgroundColor = colors;
+    }
+    colors[dataArray.length - i - 1] = '#7cc746';
+    chart.data.datasets[0].backgroundColor = colors;
+    chart.update();
+  }
+};
+
+module.exports = {
+  shuffle: shuffle,
+  bubbleSort: bubbleSort
+};
